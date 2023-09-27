7e31d9fa8ff146fa83e2c4eea37f8dda4381fb2c
kakeru Aoyama <kakeru0520sou1@gmail.com>
2023-09-27 14:18:14 +0900

    rspecの導入

---
+++ rails/spec/requests/api/v1/health_check_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Api::V1::HealthCheck", type: :request do
+  describe "GET api/v1/health_check" do
+    subject { get(api_v1_health_check_path) }
+
+    before { subject }
+
+    describe "response body" do
+      let(:parsed_response) { JSON.parse(response.body) }
+
+      it "returns a success message" do
+        expect(parsed_response["message"]).to eq "Success Health Check!"
+      end
+    end
+
+    describe "response status" do
+      it "returns a success status" do
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
