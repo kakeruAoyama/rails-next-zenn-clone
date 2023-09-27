361419b1feb89237566e772695faa59e486acf75
kakeru Aoyama <kakeru0520sou1@gmail.com>
2023-09-27 14:29:55 +0900

    rubocipの導入

--- rails/spec/requests/api/v1/health_check_spec.rb
+++ rails/spec/requests/api/v1/health_check_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "Api::V1::HealthCheck", type: :request do
+RSpec.describe "Api::V1::HealthCheck" do
   describe "GET api/v1/health_check" do
     subject { get(api_v1_health_check_path) }
 
     before { subject }
 
     describe "response body" do
-      let(:parsed_response) { JSON.parse(response.body) }
+      let(:parsed_response) { response.parsed_body }
 
       it "returns a success message" do
         expect(parsed_response["message"]).to eq "Success Health Check!"
